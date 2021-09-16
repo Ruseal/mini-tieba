86264529ced5acc2d859a72008ebb89301d44f7a
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-09-16 12:25:56 +0800

    set views

---
+++ src/views/page/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <router-view />
+    <tab-bar />
+  </div>
+</template>
+
+<script>
+import TabBar from "@/components/common/TabBar.vue";
+export default {
+  name: "page",
+  components: { TabBar },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
