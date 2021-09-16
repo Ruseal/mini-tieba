fe146d97dd750ddfa97a46d5c5a7a4312d8888ae
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-09-16 12:30:07 +0800

    提交登录页面

---
+++ src/views/login/RegisterSetPassword.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register-set-password">
+    <login-component />
+  </div>
+</template>
+
+<script>
+import LoginComponent from "@/components/common/LoginComponent.vue";
+export default {
+  name: "",
+  components: { LoginComponent },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
---
+++ src/views/login/RegisterUser.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <login-component />
+  </div>
+</template>
+
+<script>
+import LoginComponent from "@/components/common/LoginComponent.vue";
+export default {
+  name: "",
+  components: { LoginComponent },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
