ddd98fea11f2206df0f246a8b66e6450f611d3c5
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-09-16 12:36:44 +0800

    commit global

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import router from './router'
 import store from './store'
 import 'amfe-flexible'
 import 'fastclick'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import VueLazyLoad from 'vue-lazyload'
+import 'element-ui/lib/theme-chalk/index.css';
+import {
+  Switch
+} from 'element-ui';
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
+Vue.use(VueLazyLoad)
+Vue.use(Switch)
 
 new Vue({
   router,
