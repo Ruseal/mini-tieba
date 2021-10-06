38226f74e743dea96d1bd61116f6701d0659b512
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-10-06 21:38:11 +0800

    methods

---
+++ src/methods/common-methods.js
@@ -0,0 +1,15 @@
+import { isLogin } from '../api/user-net'
+
+export const isLoginMethod = async (_this) => {
+  try {
+    const { status } = await isLogin()
+    if(status===401) {
+      _this.$toast.fail('您还未登入哦')
+      return
+    }
+    if(status!==200) throw new Error()
+    return true
+  } catch (err) {
+    _this.$toast.fail('网络不稳定，请稍后再尝试')
+  }
+}
