1d8a513db84d07b2627995856d2c5d412e5c317c
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-11-20 19:48:05 +0800

    搜索界面组件

---
+++ src/components/content/search/SearchHistory.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class='search-history'>
+   
+ </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {
+      
+    };
+  },
+}
+</script>
+<style lang='less' scoped>
+</style>
