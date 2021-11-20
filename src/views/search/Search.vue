103c1ece9e084ac59f157667f3291845b3bf52ff
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-11-20 19:47:29 +0800

    搜索界面

---
+++ src/views/search/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search">
+    <nav-search />
+  </div>
+</template>
+
+<script>
+import NavSearch from "../../components/content/search/NavSearch.vue";
+export default {
+  name: "",
+  components: { NavSearch },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
