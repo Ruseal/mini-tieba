055f4d6742eee676c71036e6c42895994140ca8a
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-12-13 23:45:27 +0800

    add Empty components

---
+++ src/components/common/CommentEmpty.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="comment-empty">
+    <div class="nav">
+      <left-arrow @click.native="$router.back()" class="arrow" />
+      <div>{{ $route.query.name}}</div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftArrow from "./LeftArrow.vue";
+export default {
+  name: "",
+  components: { LeftArrow },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang='less' scoped>
+.comment-empty {
+  .nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 46px;
+    .arrow {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
